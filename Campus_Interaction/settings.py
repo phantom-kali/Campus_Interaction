11a34a0716e82cf2da20f8501ffd4982f0367af1
Benard Karanja <152717293+benard00118@users.noreply.github.com>
2024-11-20 12:01:24 +0300

    Update settings.py

--- Campus_Interaction/settings.py
+++ Campus_Interaction/settings.py
@@ -144,9 +144,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "camphub.ke@gmail.com"
-EMAIL_HOST_PASSWORD = "daqz qzkm ximp xpiy"
-DEFAULT_FROM_EMAIL = "camphub.ke@gmail.com"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = ""
 
 # Authentication Backend
 AUTHENTICATION_BACKENDS = [
